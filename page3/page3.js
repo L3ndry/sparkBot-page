655dcacf1c1e4cdd7b596f0078e967fdbad534bc
L3ndry <leandro.loginw@gmail.com>
2022-12-05 15:57:34 -0300

    9 pages

---
+++ page3/page3.js
@@ -0,0 +1,20 @@
+const label = document.querySelector('#offerChoose');
+const offer1 = document.querySelector('.offersBlock1');
+const offer2 = document.querySelector('.offersBlock2');
+
+window.onload = changeOffer();
+
+function changeOffer() {
+    let selected = label.options[label.selectedIndex].text;
+    if (selected === "Plano 1") {
+        label.style.background = "#414141";
+        offer2.style.display = "none";
+        offer1.classList.remove("d-none")
+        offer1.classList.add("d-flex flex-column")
+    } else if (selected === "Plano 2") {
+        label.style.background = "#9022eb";
+        offer1.classList.add("d-none")
+        offer2.style.display = "flex";
+        offer2.style.flexDirection = "column";
+    }
+}
