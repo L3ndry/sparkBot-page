8807e4ce085ce469d379190fee0350638e0ee064
L3ndry <leandro.loginw@gmail.com>
2022-12-20 18:02:42 -0300

    js removed

--- page3/script.js
+++ page3/script.js
@@ -1,20 +1,20 @@
-const label = document.querySelector('#offerChoose');
-const offer1 = document.querySelector('.offersBlock1');
-const offer2 = document.querySelector('.offersBlock2');
+// const label = document.querySelector('#offerChoose');
+// const offer1 = document.querySelector('.offersBlock1');
+// const offer2 = document.querySelector('.offersBlock2');
 
-label.style.backgroundColor = '#444444';
+// label.style.backgroundColor = '#444444';
 
-function changeOffer() {
-    let selected = label.options[label.selectedIndex].text;
-    if (selected === "Plano 1") {
-        label.style.background = "#414141";
-        offer2.style.display = "none";
-        offer1.classList.remove("d-none")
-        offer1.classList.add("d-flex flex-column")
-    } else if (selected === "Plano 2") {
-        label.style.background = "#9022eb";
-        offer1.classList.add("d-none")
-        offer2.style.display = "flex";
-        offer2.style.flexDirection = "column";
-    }
-}
+// function changeOffer() {
+//     let selected = label.options[label.selectedIndex].text;
+//     if (selected === "Plano 1") {
+//         label.style.background = "#414141";
+//         offer2.style.display = "none";
+//         offer1.classList.remove("d-none")
+//         offer1.classList.add("d-flex flex-column")
+//     } else if (selected === "Plano 2") {
+//         label.style.background = "#9022eb";
+//         offer1.classList.add("d-none")
+//         offer2.style.display = "flex";
+//         offer2.style.flexDirection = "column";
+//     }
+// }
